c6f27578b97839bcedfe29a7432cbc9d7f0dd167
Fuadseid <fuaddbus@gmail.com>
2024-10-16 09:30:07 +0300

    Deploying to production

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -7,7 +7,6 @@ import { FaSquareUpwork } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-
 const Contact = ({ isLight }) => {
   const form = useRef();
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -44,15 +43,18 @@ const Contact = ({ isLight }) => {
   return (
     <div className={isLight ? styles.mainlight : styles.maindark}>
       <section className={styles.sec}>
-        <div  className={isLight ? styles.container : styles.darkcontainer}>
+        <div className={isLight ? styles.container : styles.darkcontainer}>
           <motion.form
-    initial={{y:"100%"}}
-    animate={{y:0}}
-    exit={{y:"-100%"}}
-    transition={{duration:0.7,delay:0}}
-    className={isLight ? styles.formco : styles.formcodark} ref={form} onSubmit={sendEmail}>
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "-100%" }}
+            transition={{ duration: 0.7, delay: 0 }}
+            className={isLight ? styles.formco : styles.formcodark}
+            ref={form}
+            onSubmit={sendEmail}
+          >
             <label>Name</label>
-            <input type="text" name="user_name" required />
+            <input type="text" name="from_name" required />
             <label>Email</label>
             <input type="email" name="user_email" required />
             <label>Message</label>
@@ -62,29 +64,28 @@ const Contact = ({ isLight }) => {
               value={isSuccess ? "Success" : "Send"}
               disabled={isSubmitting || isSuccess}
             />
-                 {isSuccess && (
-            <button className={styles.button} onClick={handleReset}>Send Another Message</button>
-          )}
-         <div   className={
-                  isLight ? styles.socialmedia : styles.socialmediadark
-                }>
-                      <a href="https://github.com/Fuadseid">
-                  <FaGithub />
-                </a>
-                <a href="https://linkedin.com/in/fuad-seid-8b61322a9">
-                  <CiLinkedin />
-                </a>
-                <a href="https://upwork.com/freelancers/~01da3f6dc0c1cdd8d4">
-                  <FaSquareUpwork />
-                </a>
-                <a href="https://instagram.com/fuadseidm07/">
-                  <FaInstagram />
-                </a>
-          </div>
-
+            {isSuccess && (
+              <button className={styles.button} onClick={handleReset}>
+                Send Another Message
+              </button>
+            )}
+            <div
+              className={isLight ? styles.socialmedia : styles.socialmediadark}
+            >
+              <a href="https://github.com/Fuadseid">
+                <FaGithub />
+              </a>
+              <a href="https://linkedin.com/in/fuad-seid-8b61322a9">
+                <CiLinkedin />
+              </a>
+              <a href="https://upwork.com/freelancers/~01da3f6dc0c1cdd8d4">
+                <FaSquareUpwork />
+              </a>
+              <a href="https://instagram.com/fuadseidm07/">
+                <FaInstagram />
+              </a>
+            </div>
           </motion.form>
- 
-
         </div>
       </section>
     </div>
